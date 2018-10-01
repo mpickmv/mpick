d392f83731c69bc3767b8a5de69dc6d29ae282b2
mpickmv <43762623+mpickmv@users.noreply.github.com>
2018-10-01 20:49:59 +0100

    First upload of mpick.cs
    First upload.

---
+++ mpick.cs
@@ -0,0 +1,46 @@
+// MPick project v1.0
+// Developed using Developer Command Prompt for VS2015 and Visual Studio Code.
+// Compile using csc mpick.cs and then run with mpick.exe.
+// - Martin Sanders.
+
+using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+class bootMPick
+{
+    static void Main()
+    {
+        // Startup MPick by calling MPickStartPoint to boot the system and load files into memory.
+        MPickMainClass bootMPickMainClass = new MPickMainClass();
+        bootMPickMainClass.MPickStartPoint();
+    }
+}
+
+
+public class MPickMainClass
+{
+    public void MPickStartPoint()
+    {
+        // Login presented upon MPick bootup. This method carries out authentication.
+        string myLogin;
+        string myPassword;
+        Console.Write("\nStarting MPick...\n");
+        Console.Write("login: ");
+        myLogin = Console.ReadLine();
+        Console.Write("password: ");
+        myPassword = Console.ReadLine(); 
+
+        var lines = File.ReadAllLines("LOGIN.txt");
+        foreach (var line in lines)
+        {
+            if (line.StartsWith(myLogin))
+            {
+                Console.WriteLine(line);
+            }
+        }
+        Console.Write("\nMPick shuting down...\n\nPress enter");
+        Console.ReadLine();
+    }
+}
